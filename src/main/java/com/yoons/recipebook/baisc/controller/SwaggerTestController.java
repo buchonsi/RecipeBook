08b5d6f872f686fd0c26c18822fc0c146772afac
buchonsi <buchonsi@naver.com>
2022-04-18 23:34:34 +0900

    swagger mediaType 설정 추가

--- src/main/java/com/yoons/recipebook/baisc/controller/SwaggerTestController.java
+++ src/main/java/com/yoons/recipebook/baisc/controller/SwaggerTestController.java
@@ -1,7 +1,6 @@
 package com.yoons.recipebook.baisc.controller;
 
 import com.yoons.recipebook.baisc.dto.SwaggerTestDto;
-import io.swagger.v3.oas.annotations.Hidden;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.Parameters;
@@ -11,7 +10,6 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Tag(name = "SwaggerTest", description = "Swagger 테스트")
@@ -22,7 +20,7 @@ public class SwaggerTestController {
 
     @Operation(summary = "get swaggerTest", description = "테스트 응답받기")
     @ApiResponses({
-            @ApiResponse(responseCode = "200", description = "OK", content = @Content(schema = @Schema(implementation = SwaggerTestDto.class))),
+            @ApiResponse(responseCode = "200", description = "OK", content = @Content(mediaType = "application/json", schema = @Schema(implementation = SwaggerTestDto.class))),
             @ApiResponse(responseCode = "400", description = "BAD REQUEST", content = @Content(schema = @Schema(hidden = true))),
             @ApiResponse(responseCode = "404", description = "NOT FOUND", content = @Content(schema = @Schema(hidden = true))),
             @ApiResponse(responseCode = "500", description = "INTERNAL SERVER ERROR", content = @Content(schema = @Schema(hidden = true)))
