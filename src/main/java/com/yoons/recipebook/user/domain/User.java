35ffc58177bed026d9c5b5d50c461d04ec2052ba
buchonsi <buchonsi@naver.com>
2022-04-25 22:49:49 +0900

    User Db 연동

---
+++ src/main/java/com/yoons/recipebook/user/domain/User.java
@@ -0,0 +1,34 @@
+package com.yoons.recipebook.user.domain;
+
+import com.yoons.recipebook.baisc.domain.BaseTimeEntity;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@Builder
+public class User extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long UserId;
+
+    @Column(nullable = false)
+    private String name;
+
+    private String email;
+
+    public User() {
+    }
+
+    public User(Long userId, String name, String email) {
+        UserId = userId;
+        this.name = name;
+        this.email = email;
+    }
+}
