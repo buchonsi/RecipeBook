8b2e6692a37a7bd02b4487067c433af48d54a4b4
buchonsi <buchonsi@naver.com>
2022-04-25 22:59:46 +0900

    user reg컬럼 삭제, test코드 작성필요

--- src/main/java/com/yoons/recipebook/user/domain/User.java
+++ src/main/java/com/yoons/recipebook/user/domain/User.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.sql.Timestamp;
-import java.time.LocalDateTime;
 
 @Entity
 @Getter @Setter
---
+++ src/test/java/com/yoons/recipebook/user/domain/UserTest.java
@@ -0,0 +1,8 @@
+package com.yoons.recipebook.user.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    //@TODO user생성시 시간 자동으로 생성되는지 테스트 해야함
+}
