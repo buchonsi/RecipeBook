131f55e3b14b7cfdf0a5f57f52fb88d408b90cf5
buchonsi <buchonsi@naver.com>
2022-04-28 21:22:06 +0900

    임시생성

---
+++ src/main/java/com/yoons/recipebook/user/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.yoons.recipebook.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    //신규
+    //저장
+    //수정
+    //삭제
+
+}
