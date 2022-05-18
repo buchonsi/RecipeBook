7e3bc323ae20b27d0c1aada0b4de9070e1213fa3
buchonsi <buchonsi@naver.com>
2022-05-18 10:00:22 +0900

    user entity update

--- src/main/java/com/yoons/recipebook/model/User.java
+++ src/main/java/com/yoons/recipebook/model/User.java
@@ -1,36 +1,30 @@
 package com.yoons.recipebook.model;
 
 import com.yoons.recipebook.baisc.domain.BaseTimeEntity;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User extends BaseTimeEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long UserId;
+    private Long id;
 
     @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
+    private String userId;
 
     @Column(nullable = false)
     private String password;
 
     private String email;
 
-    public User(){
-    }
-
-    public User(Long userId, String name, String password, String email) {
-        UserId = userId;
-        this.name = name;
-        this.password = password;
-        this.email = email;
-    }
 }
