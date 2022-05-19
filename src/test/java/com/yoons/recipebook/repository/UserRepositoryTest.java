0e9ec6856f6933d12a1877d30d785ff6430460c3
buchonsi <buchonsi@naver.com>
2022-05-19 09:17:15 +0900

    com2 test ok

--- src/test/java/com/yoons/recipebook/repository/UserRepositoryTest.java
+++ src/test/java/com/yoons/recipebook/repository/UserRepositoryTest.java
@@ -3,14 +3,8 @@ package com.yoons.recipebook.repository;
 import com.yoons.recipebook.model.User;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-
-import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
